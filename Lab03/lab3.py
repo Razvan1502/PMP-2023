06c46ef4df8896f8651877a157ba31b8faf2db75
Carp Razvan Nicolae <100423080+Razvan1502@users.noreply.github.com>
2023-10-17 13:49:20 +0300

    lab3

--- Lab03/lab3.py
+++ Lab03/lab3.py
@@ -3,12 +3,12 @@ from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
 
 
-model = BayesianNetwork([('Cutremur', 'Incendiu'), ('Cutremur', 'Alarma'), ('Incendiu', 'Alarma')])
+model = BayesianNetwork([('cutremur', 'incendiu'), ('cutremur', 'alarma'), ('incendiu', 'alarma')])
 
 # tabelele CPD
-cpd_cutremur = TabularCPD(variable='Cutremur', variable_card=2, values=[[0.9995], [0.0005]])
-cpd_incendiu = TabularCPD(variable='Incendiu', variable_card=2, values=[[0.99, 0.97], [0.01, 0.03]], evidence=['Cutremur'], evidence_card=[2])
-cpd_alarmă = TabularCPD(variable='Alarma', variable_card=2, values=[[0.9999, 0.05, 0.98 , 0.02], [0.0001, 0.95, 0.02, 0.98]], evidence=['Incendiu', 'Cutremur'], evidence_card=[2, 2])
+cpd_cutremur = TabularCPD(variable='cutremur', variable_card=2, values=[[0.9995], [0.0005]])
+cpd_incendiu = TabularCPD(variable='incendiu', variable_card=2, values=[[0.99, 0.97], [0.01, 0.03]], evidence=['cutremur'], evidence_card=[2])
+cpd_alarmă = TabularCPD(variable='alarma', variable_card=2, values=[[0.9999, 0.05, 0.98 , 0.02], [0.0001, 0.95, 0.02, 0.98]], evidence=['incendiu', 'cutremur'], evidence_card=[2, 2])
 
 # adaugam CPD la retea
 model.add_cpds(cpd_cutremur, cpd_incendiu, cpd_alarmă)
@@ -20,10 +20,10 @@ model.check_model()
 inference = VariableElimination(model)
 
 # probabil. ca a avut loc un cutremur daca alarma a fost declansata
-result = inference.query(variables=['Cutremur'], evidence={'Alarma': 1})
+result = inference.query(variables=['cutremur'], evidence={'alarma': 1})
 
 print(result)
-
-result = inference.query(variables=['Incendiu'] , evidence={'Alarma':0})
+#daca alarma  nu a fost declansata
+result = inference.query(variables=['incendiu'] , evidence={'alarma': 0})
 
 print(result)
